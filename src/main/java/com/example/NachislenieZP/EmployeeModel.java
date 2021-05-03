6b8deec0da8b25db12ad642288533d9af23d129e
IgrayuVminecraftKakBozhenka <danko200028@mail.ru>
2021-05-03 20:53:36 +0600

    employee model done

--- src/main/java/com/example/NachislenieZP/EmployeeModel.java
+++ src/main/java/com/example/NachislenieZP/EmployeeModel.java
@@ -1,4 +1,26 @@
 package com.example.NachislenieZP;
 
-public class EmployeeModel {
+public final class EmployeeModel {
+    private final String name;
+    private final String post;
+    private final int salary;
+
+    public EmployeeModel(String name, String post, int salary) {
+        this.name = name;
+        this.post = post;
+        this.salary = salary;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
